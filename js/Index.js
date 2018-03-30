435e83bfb8bbddf73c64d4f461cf15ac98caf83a
hazel.of.the.may <hazelmay094@gmail.com>
2018-03-30 20:22:33 +0100

    Sortin CSS for search bar, but need to make it to a link

--- js/Index.js
+++ js/Index.js
@@ -6,4 +6,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+// this is for the button //
+
+var input = document.getElementById("search-postcode");
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("search-postcode").click();
+    }
+});
+
+
 </script>
